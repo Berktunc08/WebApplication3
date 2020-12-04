ebf0708cd9968c41b5135f05219ba0a3b8c15027
Berk Tunç <berk.tunc@ogr.sakarya.edu.tr>
2020-12-04 20:43:33 +0300

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+' denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+' öznitelik değerlerini değiştirin.
+
+' Derleme özniteliklerinin değerlerini incele
+<Assembly: AssemblyTitle("WebApplication3")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication3")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Aşağıdaki GUID, bu proje COM'a gösterildiğinde typelib Kimliği içindir.
+<Assembly: Guid("36a53a59-cc9f-41f6-b0be-cb145439025d")>
+
+' Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Tüm değerleri belirtebilirsiniz veya Yapı ve Düzeltme Numaralarına aşağıda 
+' gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
